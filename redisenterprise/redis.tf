daab728459428f099ad933621b62baca71432b7b
Jude Chen <judechen@microsoft.com>
2025-05-22 14:05:31 -0500

    explicit dependency on network resources.

--- redisenterprise/redis.tf
+++ redisenterprise/redis.tf
@@ -4,6 +4,20 @@ resource "azurerm_redis_enterprise_cluster" "redisent-primary" {
   location            = local.primary_region
 
   sku_name = "Enterprise_E5-2"
+  depends_on = [
+    module.redis-private-dns-zone,
+    # module.redis-private-dns-zone.azurerm_private_dns_zone_virtual_network_link.this["primary"],
+    # module.redis-private-dns-zone.azurerm_private_dns_zone_virtual_network_link.this["secondary"],
+    # module.redis-private-dns-zone.azurerm_private_dns_zone_virtual_network_link.this["agent"],
+    module.redisent-vnet-primary,
+    module.redisent-vnet-secondary
+    # module.redisent-vnet-primary.module.peering["${local.prefix}-vnet-primary-to-agent"].azapi_resource.this[0],
+    # module.redisent-vnet-primary.module.peering["${local.prefix}-vnet-primary-to-agent"].azapi_resource.reverse[0],
+    # module.redisent-vnet-secondary.module.peering["${local.prefix}-vnet-secondary-to-agent"].azapi_resource.this[0],
+    # module.redisent-vnet-secondary.module.peering["${local.prefix}-vnet-secondary-to-agent"].azapi_resource.reverse[0],
+    # module.redisent-vnet-secondary.module.peering["primary-to-secondary"].azapi_resource.this[0],
+    # module.redisent-vnet-secondary.module.peering["primary-to-secondary"].azapi_resource.reverse[0],
+  ]
 }
 
 resource "azurerm_private_endpoint" "redisent-pe-primary" {
